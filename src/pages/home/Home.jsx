345ebb17a42bebed51ae56c6ab5700a90942c13e
Shonazarov-Sanjarbek <shonazarovsanjarbek71@gmail.com>
2024-08-20 19:41:43 +0500

    start

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Child from './Child'
+
+const Home = () => {
+  return (
+    <div>
+      <h2 className='text-center text-4xl'>Home</h2>
+      <Child/>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/pages/wishlist/Wishlist.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Wishlist = () => {
+  return (
+    <div >
+      <h2 className='text-center text-4xl'>wishlist</h2>
+    </div>
+  )
+}
+
+export default Wishlist
